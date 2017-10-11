f57eb1e3abba35e22474c7dbe96c2a1e148037fc
Garret Tonra <gtonra89@gmail.com>
2017-10-11 17:13:21 +0100

    added project files

---
+++ Assets/Scripts/Background Script/BGScaler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScaler : MonoBehaviour {
+	
+	void Start () {
+		var height = Camera.main.orthographicSize * 2f; //calculating Screen height so it can render for any size
+		var widht = height * Screen.width / Screen.height; //calculating Screen width so it can render for any size
+
+		if (gameObject.name == "Background"){ 
+			transform.localScale = new Vector3 (widht, height, 0);
+		} else {
+			transform.localScale = new Vector3 (widht + 3f, 6, 0);
+		}
+
+	}
+
+}
